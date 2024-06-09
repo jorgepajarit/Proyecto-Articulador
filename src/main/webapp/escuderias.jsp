2d3ea659502ebc6224d8dbcf60f37a2a660c6859
Juancasta14 <juanfelipe9902@gmail.com>
2024-06-08 22:30:30 -0500

    Incorporacion base de datos

--- src/main/webapp/escuderias.jsp
+++ src/main/webapp/escuderias.jsp
@@ -6,6 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Escuderias</title>
+    <link rel="icon" href="assets/logos.png" type="image/x-icon">
     <link rel="stylesheet" href="styles/escuderias.css">
 </head>
 <body>
--- src/main/webapp/js/circuitos.js
+++ src/main/webapp/js/circuitos.js
@@ -1,40 +1,44 @@
-
-function pausarYReiniciarVideo() {
-    // Pausar el video
-    var videoIframe = document.querySelector('.video-circuito iframe');
-    videoIframe.contentWindow.postMessage('{"event":"command","func":"pauseVideo","args":""}', '*');
-    
-    // Reiniciar el video al principio
-    videoIframe.contentWindow.postMessage('{"event":"command","func":"seekTo","args":[0,true]}', '*');
-}
-
-
 document.addEventListener('DOMContentLoaded', function () {
     const circuitos = document.querySelectorAll('#circuitos .circuitos-lista li');
     circuitos.forEach((circuito, index) => {
-      circuito.addEventListener('click', () => {
-        document.querySelectorAll('.circuitos-info').forEach(info => {
-          info.style.display = 'none';
-        });
-        const info = circuito.querySelector('.circuitos-info');
-        const cerrar = info.querySelector('.cerrar');
+        circuito.addEventListener('click', () => {
+            document.querySelectorAll('.circuitos-info').forEach(info => {
+                info.style.display = 'none';
+            });
+            const info = circuito.querySelector('.circuitos-info');
 
-        if (info) {
-          info.style.display = 'block';
-        }
-        cerrar.addEventListener('click', (event) => {
-          event.stopPropagation(); // Evitar que se cierre al hacer clic en el botón de cerrar
-          pausarYReiniciarVideo();
-          info.style.display = 'none';
-          
-        
+            if (info) {
+                info.style.display = 'block';
+            }
         });
-      });
     });
-  });
 
+    // Agregar evento de clic para cerrar dentro del ciclo de circuitos
+    const cerrarBotones = document.querySelectorAll('#cerrar');
+    cerrarBotones.forEach((cerrar) => {
+        cerrar.addEventListener('click', () => {
+            cerrar.stopPropagation();
+            pausarYReiniciarVideo();
+            const circuitoInfo = cerrar.closest('.circuitos-info');
+            circuitoInfo.style.display = 'none';
+        });
+    });
+});
 
-  window.addEventListener("load", () => {
+function pausarYReiniciarVideo() {
+    // Obtener el elemento de video dentro del iframe
+    var video = document.querySelector('.video-circuito iframe').contentDocument.querySelector('video');
+
+    // Verificar si se encontró el elemento de video
+    if (video) {
+        // Pausar el video
+        video.pause();
+        // Reiniciar el video al principio
+        video.currentTime = 0;
+    }
+}
+
+window.addEventListener("load", () => {
     const currentDate = new Date();
     document.getElementById("year").innerText = currentDate.getFullYear();
-  })
+});
